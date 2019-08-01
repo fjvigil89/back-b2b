1185b5e91f4b1250232196c85ba7a4919ddb7ac9
Francisco Javier Sánchez Fuentes <fransafu@gmail.com>
2019-08-01 17:55:25 -0400

    fix(CI/CD): Add emal and username for git

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -30,6 +30,9 @@ printf "[profile eb-cli]\nregion=%s\noutput=json" "$AWS_EB_REGION" >> ~/.aws/con
 
 ./deploy/generateConfig.sh
 
+git config --global user.email "cicd@cademsmart.cl"
+git config --global user.name "Runner gitlab for b2b-back"
+
 git add .ebextensions/node-settings.config
 git commit -m "Load env vars"
 
