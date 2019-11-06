8549756060b96734c03525b8d4b9b5cbbb86bf3b
Francisco Sanchez Fuentes <fransafu@gmail.com>
2019-11-06 13:00:45 -0300

    fix(SUPI): fix Syntax error query supi

--- src/services/external/SUPI.ts
+++ src/services/external/SUPI.ts
@@ -39,12 +39,12 @@ function ultimasVisitas(folio: number): Promise<Array<{ id_visita: number, fecha
         SELECT TOP 2 a.ID_VISITA as id_visita
             , a.HORAINICIO as fecha
             , a.ESTADO as estado
-        FROM VISITA WITH(NOLOCK) a
-        INNER JOIN ESTUDIOSALA WITH(NOLOCK) b
+        FROM VISITA a WITH(NOLOCK)
+        INNER JOIN ESTUDIOSALA b WITH(NOLOCK)
             ON a.ID_ESTUDIOSALA = b.ID_ESTUDIOSALA
-        INNER JOIN SALA WITH(NOLOCK) c
+        INNER JOIN SALA c WITH(NOLOCK)
             ON b.ID_SALA = c.ID_SALA
-        INNER JOIN ESTUDIO WITH(NOLOCK) d
+        INNER JOIN ESTUDIO d WITH(NOLOCK)
             ON d.ID_ESTUDIO = b.ID_ESTUDIO
         WHERE c.FOLIOCADEM = ${folio}
             AND d.ID_ESTUDIO = ${process.env.ID_ESTUDIO_SUPI}
@@ -58,10 +58,10 @@ export function tomaVisita(visitaId: number): Promise<IToma[]> {
             , b.DESCRIPCION as descripcion
             , b.EAN as ean
             , c.DESCRIPCION as categoria
-        FROM TOMA WITH(NOLOCK) a
-        INNER JOIN dbo.ITEM WITH(NOLOCK) b
+        FROM TOMA a WITH(NOLOCK)
+        INNER JOIN dbo.ITEM b WITH(NOLOCK)
             ON a.ID_PRODUCTO = b.ID_ITEM
-        INNER JOIN CODIGOS WITH(NOLOCK) c
+        INNER JOIN CODIGOS c WITH(NOLOCK)
             ON b.ID_CATEGORIA = c.ID_CODIGO
         WHERE a.ID_VISITA = ${visitaId}
             AND a.ID_VARIABLE = 1
