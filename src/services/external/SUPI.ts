6107b44c010101f9f62dd51035afbff802c61e6a
Francisco Sanchez <francisco.sanchez@cademsmart.cl>
2019-12-27 09:00:39 -0300

    fix(abi): Temporal change ID_ESTUDIO ABI

--- src/services/external/SUPI.ts
+++ src/services/external/SUPI.ts
@@ -32,7 +32,7 @@ function ultimasVisitas(folio: number): Promise<Array<{ id_visita: number, fecha
         INNER JOIN ESTUDIO d WITH(NOLOCK)
             ON d.ID_ESTUDIO = b.ID_ESTUDIO
         WHERE c.FOLIOCADEM = ${folio}
-            AND d.ID_ESTUDIO = 438
+            AND d.ID_ESTUDIO = 480
             AND a.DIA >= '${moment().subtract(30, "day").format("YYYY-MM-DD")}'
         ORDER BY a.DIA DESC`));
 }
