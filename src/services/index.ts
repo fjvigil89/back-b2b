b971493c7be0c29098a90a11c49d950ebdbb6638
Nicolás Carreño <n.carrenoh2019@gmail.com>
2020-06-18 17:55:25 -0400

    Feature: (index) expor de IndicatorService

--- src/services/index.ts
+++ src/services/index.ts
@@ -6,6 +6,7 @@ import { CommentService } from "./Comment";
 import { DownloadService } from "./Download";
 import { HashtagService } from "./Hashtag";
 import { ImageService } from "./Image";
+import { Indicator as IndicadorService } from "./Indicador";
 import { ItemService } from "./Item";
 import { LikeCommentService } from "./LikeComment";
 import { LikePostService } from "./LikePost";
@@ -18,29 +19,29 @@ import { StoreService } from "./Store";
 import { SummaryService } from "./Summary";
 import { UserService } from "./User";
 import { VentasService } from "./Ventas";
-
 export {
-    CasesService,
-    CaseFeedbackService,
-    CheckService,
-    CommentService,
-    DownloadService,
-    HashtagService,
-    ImageService,
-    ItemService,
-    LikeCommentService,
-    LikePostService,
-    LikeReplyService,
-    PollService,
-    PostService,
-    ReplyService,
-    StoreService,
-    SummaryService,
-    UserService,
-    QuestionService,
-    VentasService,
-    chunk,
-    mapKeys,
-    sumBy,
-    uniqBy,
+  CasesService,
+  CaseFeedbackService,
+  CheckService,
+  CommentService,
+  DownloadService,
+  HashtagService,
+  ImageService,
+  ItemService,
+  LikeCommentService,
+  LikePostService,
+  LikeReplyService,
+  PollService,
+  PostService,
+  ReplyService,
+  StoreService,
+  SummaryService,
+  UserService,
+  QuestionService,
+  VentasService,
+  chunk,
+  mapKeys,
+  sumBy,
+  uniqBy,
+  IndicadorService,
 };
