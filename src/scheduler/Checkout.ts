bcd022f4268c5a00048aca5c3daa498047101bf1
Coding & Fitness <boadude@gmail.com>
2021-02-02 16:24:26 -0300

    Se agrega el cliente cial

--- src/scheduler/Checkout.ts
+++ src/scheduler/Checkout.ts
@@ -2,14 +2,50 @@ import { CronJob } from "cron";
 import { getConnection } from "typeorm";
 import { CheckRepository } from "../repository";
 
-export const CheckSchedulerANDINA = new CronJob("10 00 * * *", async () => {
-    await getConnection("andina").getCustomRepository(CheckRepository).closeChecks();
-}, null, null, "America/Santiago");
+export const CheckSchedulerANDINA = new CronJob(
+  "10 00 * * *",
+  async () => {
+    await getConnection("andina")
+      .getCustomRepository(CheckRepository)
+      .closeChecks();
+  },
+  null,
+  null,
+  "America/Santiago",
+);
 
-export const CheckSchedulerPERNOD = new CronJob("32 00 * * *", async () => {
-    await getConnection("pernod").getCustomRepository(CheckRepository).closeChecks();
-}, null, null, "America/Santiago");
+export const CheckSchedulerPERNOD = new CronJob(
+  "32 00 * * *",
+  async () => {
+    await getConnection("pernod")
+      .getCustomRepository(CheckRepository)
+      .closeChecks();
+  },
+  null,
+  null,
+  "America/Santiago",
+);
 
-export const CheckSchedulerICB = new CronJob("42 00 * * *", async () => {
-    await getConnection("icb").getCustomRepository(CheckRepository).closeChecks();
-}, null, null, "America/Santiago");
+export const CheckSchedulerICB = new CronJob(
+  "42 00 * * *",
+  async () => {
+    await getConnection("icb")
+      .getCustomRepository(CheckRepository)
+      .closeChecks();
+  },
+  null,
+  null,
+  "America/Santiago",
+);
+
+export const CheckSchedulerCIAL = new CronJob(
+  "52 00 * * *",
+  async () => {
+    await getConnection("cial")
+      .getCustomRepository(CheckRepository)
+      .closeChecks();
+  },
+  null,
+  null,
+  "America/Santiago",
+);
