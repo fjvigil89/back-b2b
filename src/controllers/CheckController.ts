efb1f4ecb1224b4af8500f8706c9a1f2c0613601
Coding & Fitness <boadude@gmail.com>
2021-09-13 18:07:02 -0300

    Limpieza

--- src/controllers/CheckController.ts
+++ src/controllers/CheckController.ts
@@ -3,40 +3,41 @@ import { CheckService } from "../services";
 import { Controller } from "./Controller";
 
 export class CheckController extends Controller {
+  private checkService: CheckService;
 
-    private checkService: CheckService;
+  constructor(req: Request, res: Response) {
+    super(req, res);
+    this.checkService = new CheckService();
+  }
 
-    constructor(req: Request, res: Response) {
-        super(req, res);
-        this.checkService = new CheckService();
+  public async create(): Promise<Response> {
+    const { folio, type } = this.req.body;
+    const { userId, client } = this.req.user;
+
+    try {
+      await this.checkService.createCheck(client, folio, userId, type);
+      return this.res.status(200).send();
+    } catch (ex) {
+      console.error("/check: ", ex);
+      return this.res.status(500).json({ message: ex.message });
     }
+  }
 
-    public async create(): Promise<Response> {
-        const { folio, type } = this.req.body;
-        const { userId, client } = this.req.user;
-        console.log("cliet: ", client);
-        try {
-            await this.checkService.createCheck(client, folio, userId, type);
-            return this.res.status(200).send();
-        } catch (ex) {
-            console.error("/check: ", ex);
-            return this.res.status(500).json({ message: ex.message });
-        }
+  public async validateCheckIn(): Promise<Response> {
+    const { folio } = this.req.body;
+    const { userId, client } = this.req.user;
+
+    try {
+      let response = await this.checkService.validateCheckIn(
+        client,
+        folio,
+        userId,
+      );
+
+      return this.res.status(200).json({ check: response }).send();
+    } catch (ex) {
+      console.error("/check: ", ex);
+      return this.res.status(500).json({ message: ex.message });
     }
-
-    public async validateCheckIn(): Promise<Response> {
-        const { folio } = this.req.body;
-        const { userId, client } = this.req.user;
-        console.log("cliet: ", client);
-        try {
-            let response = await this.checkService.validateCheckIn(client, folio, userId);
-
-            return this.res.status(200).json({ check: response }).send();
-
-        } catch (ex) {
-            console.error("/check: ", ex);
-            return this.res.status(500).json({ message: ex.message });
-        }
-    }
-
+  }
 }
