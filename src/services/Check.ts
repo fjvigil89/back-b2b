ccf071fe9b86a120f8aa181dc6b14d263ca62117
Coding & Fitness <boadude@gmail.com>
2021-09-13 18:07:34 -0300

    Limpieza v2

--- src/services/Check.ts
+++ src/services/Check.ts
@@ -4,65 +4,82 @@ import { Check } from "../entity";
 import { CheckRepository, StoreRepository } from "../repository";
 
 export class CheckService {
+  private check: Check;
 
-    private check: Check;
+  constructor() {
+    this.check = new Check();
+  }
 
-    constructor() {
-        this.check = new Check();
-    }
+  public async createCheck(
+    client: string,
+    folio: number,
+    userId: string,
+    type: string,
+  ): Promise<void> {
+    const store = await getConnection(client)
+      .getCustomRepository(StoreRepository)
+      .findByStoreId(folio);
+    if (store) {
+      const prevCheck = await getConnection(client)
+        .getCustomRepository(CheckRepository)
+        .findLastCheck(userId);
 
-    public async createCheck(client: string, folio: number, userId: string, type: string): Promise<void> {
-        const store = await getConnection(client).getCustomRepository(StoreRepository).findByStoreId(folio);
-        if (store) {
-            const prevCheck = await getConnection(client).getCustomRepository(CheckRepository).findLastCheck(userId);
-            console.log('prevCheck: ', prevCheck);
-            if (prevCheck) {
-                if (type === "in") {
-                    if (prevCheck.dateCheckOut) {
-                        await this.newCheck(client, userId, folio);
-                    } else {
-                        throw new Error("Ya existe una visita en progreso");
-                    }
-                } else if (type === "out") {
-                    if (prevCheck.dateCheckIn && prevCheck.dateCheckOut == null) {
-                        prevCheck.dateCheckOut = moment(new Date()).format("YYYY-MM-DD HH:mm:ss");
-                        await getConnection(client).getRepository(Check).save(prevCheck);
-                    } else {
-                        throw new Error("No existe ninguna visita en progreso");
-                    }
-                }
-            } else {
-                if (type === "in") {
-                    await this.newCheck(client, userId, folio);
-                } else {
-                    throw new Error("No existe ninguna visita en progreso");
-                }
-            }
-        } else {
-            throw new Error(`No existe el folio`);
+      if (prevCheck) {
+        if (type === "in") {
+          if (prevCheck.dateCheckOut) {
+            await this.newCheck(client, userId, folio);
+          } else {
+            throw new Error("Ya existe una visita en progreso");
+          }
+        } else if (type === "out") {
+          if (prevCheck.dateCheckIn && prevCheck.dateCheckOut == null) {
+            prevCheck.dateCheckOut = moment(new Date()).format(
+              "YYYY-MM-DD HH:mm:ss",
+            );
+            await getConnection(client).getRepository(Check).save(prevCheck);
+          } else {
+            throw new Error("No existe ninguna visita en progreso");
+          }
         }
-    }
-
-    private async newCheck(client: string, userId: string, folio: number): Promise<void> {
-        this.check.userId = userId;
-        this.check.folio = folio;
-        this.check.dateCheckIn = moment(new Date()).format("YYYY-MM-DD HH:mm:ss");
-        await getConnection(client).getRepository(Check).save(this.check);
-    }
-
-    public async validateCheckIn(client: string, folio: number, userId: string){
-        const store = await getConnection(client).getCustomRepository(StoreRepository).findByStoreId(folio);
-        if (store) {
-            const prevCheck = await getConnection(client).getCustomRepository(CheckRepository).findLastCheck(userId);
-            console.log('prevCheck: ', prevCheck);
-            if(prevCheck.dateCheckOut === null) {
-                return true
-            } else {
-                return false
-            }
+      } else {
+        if (type === "in") {
+          await this.newCheck(client, userId, folio);
         } else {
-            throw new Error(`No existe el folio`);
+          throw new Error("No existe ninguna visita en progreso");
         }
+      }
+    } else {
+      throw new Error(`No existe el folio`);
     }
+  }
 
+  private async newCheck(
+    client: string,
+    userId: string,
+    folio: number,
+  ): Promise<void> {
+    this.check.userId = userId;
+    this.check.folio = folio;
+    this.check.dateCheckIn = moment(new Date()).format("YYYY-MM-DD HH:mm:ss");
+    await getConnection(client).getRepository(Check).save(this.check);
+  }
+
+  public async validateCheckIn(client: string, folio: number, userId: string) {
+    const store = await getConnection(client)
+      .getCustomRepository(StoreRepository)
+      .findByStoreId(folio);
+    if (store) {
+      const prevCheck = await getConnection(client)
+        .getCustomRepository(CheckRepository)
+        .findLastCheck(userId);
+      console.log("prevCheck: ", prevCheck);
+      if (prevCheck.dateCheckOut === null) {
+        return true;
+      } else {
+        return false;
+      }
+    } else {
+      throw new Error(`No existe el folio`);
+    }
+  }
 }
